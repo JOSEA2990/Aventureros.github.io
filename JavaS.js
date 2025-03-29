36c6766fb103316b150f525e276984dd6c059610
JOSEA2990 <joseamartinezg.2990@gmail.com>
2025-03-29 15:24:37 -0500

    Update4
    Barra de seleccióm

--- JavaS.js
+++ JavaS.js
@@ -18,3 +18,12 @@ const users = [
     alert( 'Usuario o contraseña incorrectos' );
     }
     }
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        },
+    });
